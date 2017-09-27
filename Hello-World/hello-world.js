35ec55c6042a65eb5bc6e441cd5ecf7f8c336f74
theotherjonjanes <theotherjonjanes@users.noreply.github.com>
2017-09-27 14:15:25 -0400

    First commit

---
+++ Hello-World/hello-world.js
@@ -0,0 +1,9 @@
+console.log("Hello world!")
+
+const hwString = "Hello world!"
+
+const hwArray = ["H","e","l","l","o"," ","w","o","r","l","d","!"]
+
+console.log(hwString)
+
+console.log(hwArray.join(""))
